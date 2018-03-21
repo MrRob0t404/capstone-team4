c48e47256d2d7000b47b7178f93b2ee6e4694845
Newton Brooks <newtonbrooks@Newtons-MacBook-Air.local>
2018-03-21 17:10:12 -0400

    ace editor login page setup

---
+++ frontend/src/components/Site/LoginPage/loginUser.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+class LoginUser extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+
+    render(){
+        return (
+            <div>
+                </div>
+        )
+    }
+
+}
+
+export default LoginUser
