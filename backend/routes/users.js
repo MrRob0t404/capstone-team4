36552952ab91b5b6a6c571f346cfffea8577883c
mon33k <moniquemojica@ac.c4q.nyc>
2018-03-21 17:19:39 -0400

    Made functions inside of queries.js and users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { loginRequired } = require("../auth/helpers");
+const passport = require("../auth/local");
+const db = require("../db/queries");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/new", db.checkUser, db.createUser);
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  console.log('this is what the DB returned', req.user);
+  const userObject = {
+    user_id: req.user.user_id,
+    username: req.user.username,
+    user_bio: req.user.user_bio
+  }
+  res.status(200).json({
+    user: userObject,
+    message: `${req.user.username} is logged in`
+  });
+  return;
+}); 
+
+router.get("/", loginRequired, db.getSingleUser,   );
+router.get("/new", db.getAllUsers);
+router.post("/new", db.createUser);
+router.get("/logout", loginRequired, db.logoutUser);
+router.get("/profile", loginRequired, db.getSingleUser);
+router.get("/")
+
 
 module.exports = router;
