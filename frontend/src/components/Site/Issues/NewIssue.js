a778d4b957c780c8be3a1459d1c9717637200b35
mon33k <moniquemojica@ac.c4q.nyc>
2018-04-04 22:52:22 -0400

    Finished file upload to cap it at 5 files at a time

--- frontend/src/components/Site/Issues/NewIssue.js
+++ frontend/src/components/Site/Issues/NewIssue.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import axios, { post } from 'axios';
 
 class NewIssue extends Component {
@@ -16,24 +16,31 @@ class NewIssue extends Component {
   //here send to backend as post request
   onFormSubmit = (e) => {
     const { clickHandler, renderNextPageFileUpload } = this.props
-    const {fileObj, fileNames} = this.state
+    const { fileObj, fileNames } = this.state
     e.preventDefault() // Stop form submit
-    if(fileObj){
+    if (fileObj) {
       renderNextPageFileUpload(fileObj, fileNames)
+
     } else {
       clickHandler()
     }
     console.log("this.state.file onFormSubmit", this.state.file)
+    var size = Object.keys(fileObj).length;
+    console.log("size", size)
+    // } else if (this.state.fileNames.length >= 5) {
+    //   return console.log("exceeded file amount")
+    // (Object.keys(fileObj).length <= 5)
   }
 
   onChange(e) {
+    const { fileObj } = this.state
     const file = e.target.files[0]
     console.log("e.target.files onChange", e.target.files)
 
     var reader = new FileReader();
     reader.onload = (e) => {
       this.setState({
-        fileObj: {...this.state.fileObj, [file.name] : e.target.result},
+        fileObj: { ...this.state.fileObj, [file.name]: e.target.result },
         fileNames: [...this.state.fileNames, file.name]
       })
     }
@@ -42,9 +49,9 @@ class NewIssue extends Component {
   }
 
   render() {
-    console.log("this.state", this.state)
-    const {inputHandler, clickHandler, message} = this.props;
-    const {fileObj} = this.state;
+    console.log("this.state NewIssue.js", this.state)
+    const { inputHandler, clickHandler, message } = this.props;
+    const { fileObj, fileNames } = this.state;
     return (
       <form id="newIssue">
         <div className="input-container">
@@ -52,21 +59,22 @@ class NewIssue extends Component {
             <h3>Title</h3>
             <p>ex. Whenever I rerender my routes dont work.</p>
           </div>
-          <input name="title" onChange={inputHandler} autofocus="true"/>
+          <input name="title" onChange={inputHandler} autofocus="true" />
         </div>
         <div className="input-container">
           <div className="helper">
             <h3>Repository</h3>
             <p>ex. https://github.com/username/project</p>
           </div>
-          <input name="repositoryLink" onChange={inputHandler}/>
+          <input name="repositoryLink" onChange={inputHandler} />
         </div>
         <div className="input-container">
           <div className="helper">
             <h3>File Upload</h3>
           </div>
-          <input type="file" onChange={this.onChange}/>
-          <p>{fileObj && Object.keys(fileObj).map(fileName => <div key={fileName}> {fileName}</div>)}</p>
+          <input type="file" onChange={this.onChange} />
+          <p>{fileNames.map(fileName => <div key={fileName}> {fileName}</div>)}</p>
+          <p>{fileNames.length >= 5 ? <div>You have exceeded the max file upload amount</div> : ''}</p>
         </div>
 
         <p className="message">{message}</p>
@@ -95,3 +103,9 @@ export default NewIssue
 //     console.log('successfully deleted ' + req.files.path);
 //   });      
 // });
+
+// if (fileName.length <= 5) {
+//   return (<div key={fileName}> {fileName}</div>)
+// } else {
+//   return (<div>You have exceeded the max file upload amount</div>)
+// }
