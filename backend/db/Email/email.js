35c9862f48547a2a3d7b3ec24f875acf84694aa8
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-04-08 13:55:31 -0400

    added css style changes; status: in progress

--- backend/db/Email/email.js
+++ backend/db/Email/email.js
@@ -12,9 +12,10 @@ const welcome = (user) => {
            <style>
            #container{text-align: center; width: 700px;}
            .logo{ background-color: #272633; width: 100%; margin: 0; padding: 0; padding-top: 20px; padding-bottom: 20px;}
-           #welcomeMessage{ background-color: grey; padding-top: 20px; padding-bottom: 20px; margin:0; width: 100%;}
-           a{background-color: #7FD017;}
-           footer{background-color: #272633; margin: 0; color: white; font-size: 12px; padding-top: 20px; padding-bottom: 20px;}
+           img{}
+           #welcomeMessage{ background-color: white; color: #272633; padding-top: 20px; padding-bottom: 20px; margin:0; width: 100%;}
+           a{background-color: #7FD017; color: #00C1AB; width: 80px; text-decoration: none; }
+           footer{background-color: #272633; margin: 0; color: white; font-size: 10px; padding-top: 10px; padding-bottom: 10px;}
            </style> 
        </head>
        <body>
@@ -23,7 +24,7 @@ const welcome = (user) => {
                 <img src="../../../frontend/src/Logos/full-logo.svg">
             </div>
             <div id="welcomeMessage">
-                <h1>Welcome ${user.username}! We're so glad you've decided to join us at Tyro Dev</h1>
+                <h2>Welcome ${user.username}! We're so glad you've decided to join us at Tyro Dev!</h2>
                 <a href ='#'>Click here to visit Tyro Dev!</a>
             </div>
             <div id="footer">    
@@ -52,6 +53,8 @@ const solutionNotification = (problemPoster,problemSolver) => {
            #container{text-align: center; width: 700px;}
            .logo{ background-color: #272633; width: 100%; margin: 0; padding: 0; padding-top: 20px; padding-bottom: 20px;}
            #welcomeMessage{ background-color: grey; padding-top: 20px; padding-bottom: 20px; margin:0; width: 100%;}
+            .profilePic{ width: 50px; height: 50px; }
+            .inline{display: block;}
            a{background-color: #FFF; display: block; width: 100px; text-align: center; margin-left: 350px;}
            footer{background-color: #272633; margin: 0; color: white; font-size: 12px; padding-top: 20px; padding-bottom: 20px;}
            </style> 
@@ -64,8 +67,8 @@ const solutionNotification = (problemPoster,problemSolver) => {
             <div id="welcomeMessage">
                 <h1>Hey ${problemPoster.username}! ${problemSolver.username} posted a solution to your ticket!</h1>
                 <a href ='http://localhost:3000/issues/${problemPoster.id}'>Link to question</a>
-                    <img src='${problemPoster.profile_pic}' alt="profilepic"/> ${problemPoster.title} 
-                    <img src='${problemSolver.profile_pic}' alt=""/>  
+                    <img class='profilePic inline' src='${problemPoster.profile_pic}' alt="profilepic"/> <p class='inline'>${problemPoster.title}</p> 
+                    <img class='profilePic inline'src='${problemSolver.profile_pic}' alt=""/>  
             </div>
             <div id="footer">    
                 <footer>
