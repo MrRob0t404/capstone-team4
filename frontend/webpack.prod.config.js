c41463a12ca7c098f6fc52edc8bb59004cc0e34e
Elon Jefferson <elekson9@yahoo.com>
2018-06-20 16:38:01 -0400

    heroku attempt 6

--- frontend/webpack.prod.config.js
+++ frontend/webpack.prod.config.js
@@ -35,5 +35,18 @@ module.exports = {
             }
         }
     }
-}
+},
+  module: {
+    rules: [
+      { test: /\.js?$/,
+        use: 'babel' },
+      { test: /\.scss?$/,
+        use: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        use: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: 'file'}
+    ]
+  }
 }
