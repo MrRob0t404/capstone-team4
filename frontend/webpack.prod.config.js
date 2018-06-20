838486d2d91fb8e97366a6dae4c823425aedbf74
Elon Jefferson <elekson9@yahoo.com>
2018-06-20 16:27:07 -0400

    heroku attempt 4

--- frontend/webpack.prod.config.js
+++ frontend/webpack.prod.config.js
@@ -15,12 +15,6 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
